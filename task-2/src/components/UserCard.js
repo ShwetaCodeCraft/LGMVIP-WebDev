350c246e62eee94e9ba2f07b08690ce721eecc67
Shweta Singh <singh.shweta2973@gmail.com>
2024-03-22 04:55:47 +0530

    task-2 update

---
+++ task-2/src/components/UserCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/UserCard.css';
+
+const UserCard = ({ user }) => {
+  return (
+    <div className="user-card">
+      <img src={user.avatar} alt={user.first_name} />
+      <div className="user-info">
+        <h2>{`${user.first_name} ${user.last_name}`}</h2>
+        <p>Email: {user.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
+
